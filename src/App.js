66cc875b5589f91ce8a5c7cce10e5623d19d8c3b
bennutech <bennutechco@gmail.com>
2017-10-05 00:55:29 -0700

    Simple React app with a website broken into pieces and compiledin an app.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar'
+import Header from './Header'
+import Footer from './Footer'
+import Body from './Body'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Header/>   
+        <Body/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
